41808c8fb102090eb0fd3fb257929299a426effa
nadinyates <nadinyates@users.noreply.github.com>
2018-02-17 19:12:27 +0100

    Final update after 2nd review

--- src/NumberGame.java
+++ src/NumberGame.java
@@ -1,79 +1,60 @@
 import java.util.Scanner;
 import java.util.Random; 
 
+
 public class NumberGame {
     // This game asks the user to guess the winning number in the range of 1 to 99
-    public static void main(String[] args) {
-    	boolean debug = true;
-        boolean gameWon=false;
-        boolean endGame=false;
-        int countdown=10; // this is set to the number of tries the user gets
-        
-    	if(args.length==0) {
-    	      debug = false;
-    	   }
-    	   else {
-    		   if (args [0] == "-d") { 
-    			   debug = true;
-    		   }
-    		   else  System.out.println("invalid parameter");
-   	    }
+    private static boolean gameWon=false;
+    private static int countdown=10; 
+    final private static int winningNumber = GenerateRandomNumber();
+    private static Scanner sc = new Scanner(System.in); 
+    private static boolean debug = true;
 
-        final int winningNumber = generateRandomNumber();
-        
-        if (debug) { 
-        	System.out.print(winningNumber);
-        }
-   
-        while (!endGame) {
-        	if (countdown > 0) {
-        		int guess = getuserinput(countdown);
-        		if (endGame = processGuess(guess,winningNumber)) {
-        			gameWon= true;        			
-        		}
-        	}
-    		else {	
-    			gameWon=false;
-        		endGame=true;
-    		}
-        	countdown--; 
+    public static void main(String[] args) {
+
+        Debugging();
+
+        while ((!gameWon) && countdown !=0) {
+            int guess = GetUserInput();
+            gameWon = DidUserWin(guess);
+            countdown-- ;
         }
 
         if (gameWon) {
-        	System.out.println("BINGO! Aren't you clever!!!");
+            System.out.println("BINGO! Aren't you clever!!!");
         }
         else {
-        	System.out.println("YOU LOST! Oh dear! Better luck next time.");
+            System.out.println("YOU LOST! Oh dear! Better luck next time.");
         }
 }
 
-
-	private static boolean processGuess(int guess, int winningNumber) {
-		// Check whether the user has entered the winning number
-		if (guess > winningNumber) {
-			System.out.println("No - too big");
-			return false;
+    private static boolean DidUserWin(int guess) {
+    	if (guess > winningNumber) {
+            System.out.println("No - too big");
+            return false;
         }
-        else if (guess < winningNumber) {
-        	System.out.println("No - too small");
-        	return false;
+        if (guess < winningNumber) {
+            System.out.println("No - too small");
+            return false;
         }
         else return true;
-	}
+	    }
 
-	private static int getuserinput(int NumberOfTries) {
-		// Get the input from the user
-		System.out.println("You have " + NumberOfTries + " tries left. Try to guess the number: ");
-		// this should be pulled out of the method right? so I don't keep doing  a new?
-		Scanner sc = new Scanner(System.in); 
-		int inputedGuess = sc.nextInt();
-		return inputedGuess;
-	}
+	private static int GetUserInput() {
+        System.out.println("You have " + countdown + " tries left. Try to guess the number: ");
+        int inputedGuess = sc.nextInt();
+        return inputedGuess;
+    }
 
-	private static int generateRandomNumber() {
-		// Generate the winning number 
-		Random random = new Random();
-	    int randomNumber = random.nextInt(99);
-		return randomNumber;
+	private static int GenerateRandomNumber() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(99);
+        return randomNumber;
+    }
+
+	private static void Debugging() {
+        if (debug) {
+            System.out.println(winningNumber);
+        }
 	}
 }
